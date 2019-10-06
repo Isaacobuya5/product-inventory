e4e745133daa3908df4f0c8b8ec450f872da44fd
Isaac Obuya <Szack4477@gmail.com>
2019-10-06 06:51:04 +0300

    identify and manage state in various components

--- src/components/SortableColumnHeader.js
+++ src/components/SortableColumnHeader.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../styles/components/SortableTableHeader.css';
+
 
 class SortableColumnHeader extends React.Component {
-    render() {
-        return(
-            <div></div>
-        )
-    }
+  render() {
+    let currentSort = this.props.currentSort.column === this.props.column ? this.props.currentSort.direction : false;
+    return(
+      <th>
+        {this.props.column}
+        <button
+          className={currentSort === 'asc' ? 'SortableColumnHeader-current' : ''}
+        >&#x25B2;</button>
+        <button
+          className={currentSort === 'desc' ? 'SortableColumnHeader-current' : ''}
+        >&#x25BC;</button>
+      </th>
+    );
+  }
 }
 
 export default SortableColumnHeader;
